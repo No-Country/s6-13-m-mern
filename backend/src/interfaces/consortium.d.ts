b9e7335ce6ba8eeb77ef4fcb13052ff771647fe1
Adrian <esteban.adrian.gonz@gmail.com>
2023-02-06 14:36:41 -0300

    Remove comments

--- backend/src/interfaces/consortium.d.ts
+++ backend/src/interfaces/consortium.d.ts
@@ -1,9 +1,4 @@
 import { ObjectId } from "mongoose";
-// TODO
-// Uncomment import below when push to dev
-// import { IUser } from "./User.js";
-// Replace interface types (users/admin/amenities) with their own interfaces, ex:
-// users: IUsers[]
 export interface IConsortium {
   name: string,
   address: string,
