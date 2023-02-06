c2b65e6327c9564653838c847aa5a11181c3ac3b
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-02-06 16:11:03 -0300

    layout2

---
+++ frontend/src/pages/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen">
+      <div className="h-[96px]">
+        <Header />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
