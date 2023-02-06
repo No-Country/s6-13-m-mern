4e13e532dc8fb81ff7288154655d8c996dca7ff2
Bellantra <yamilapaez2@hotmail.com>
2023-02-06 21:18:39 +0100

    prettier fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
-import express, {Request, Response} from "express"
-import cors from "cors"
-import morgan from "morgan"
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
@@ -9,24 +9,26 @@ dotenv.config()
 const app = express()
 
 // Settings
-app.set("port", process.env.PORT || 3001)
+app.set('port', process.env.PORT || 3001)
 
 // Middlewares
-app.use(morgan("dev"))
-app.use( cors({
-    origin: '*',
-    credentials: true,
-}))
-app.use(express.urlencoded({extended: true}))
+app.use(morgan('dev'))
+app.use(
+    cors({
+        origin: '*',
+        credentials: true,
+    })
+)
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
 
 // Routes
-app.get('/healthCheckApi', (_req, res:Response) => {
-    res.json({ok:"Ok"})
-  })
-app.use("/", (_req:Request, res:Response) => {
+app.get('/healthCheckApi', (_req, res: Response) => {
+    res.json({ ok: 'Ok' })
+})
+app.use('/', (_req: Request, res: Response) => {
     res.send(`Api on https://localhost:${app.get('port')}`)
 })
 
