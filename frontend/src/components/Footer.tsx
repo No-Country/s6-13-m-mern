92628c1021a29aa6e7dc900c176877ccf2b85ef7
David Castro <david.castroanaya@gmail.com>
2023-02-08 15:49:09 -0800

    Adding Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,28 +2,42 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='sticky top-[100vh]'>
-            <div>
-                <h3>Contact us</h3>
-                <ul>
-                    <li><a href="">sosconsortia@gmail.com</a></li>
-                    <li><a href="">www.sosconsortia.com</a></li>
-                    <li><a href="">54 9 4987-6511</a></li>
-                </ul>
-                <div>
-                    <h3>Follow us on social media</h3>
-                    <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    </ul>
-                </div>
-                <section>
-                    <h3>Copyright NoCountry 2023. All rights reserved.</h3>
-                    <span>CABA, Argetina</span>
-                </section>
+        // <footer className='sticky top-[100vh] text-white bg-grey footer_bottom'>
+        //     <div className='flex justify-around gap-4 w-[80%]'>
+
+        //         <div>
+        //             <h3>Contact us</h3>
+        //             <ul>
+        //                 <li className='flex gap-4'><img src="./assets/mail_logo.svg" alt="mail logo" /><a href="">sosconsortia@gmail.com</a></li>
+        //                 <li className='flex gap-4'><img src="./assets/web_logo.svg" alt="web lofo" /><a href="">www.sosconsortia.com</a></li>
+        //                 <li className='flex gap-4'><img src="./assets/phone_logo.svg" alt="phone logo" /><a href="">54 9 4987-6511</a></li>
+        //             </ul>
+        //         </div>
+        //         <div>
+        //             <h3>Follow us on social media</h3>
+        //             <ul className='flex flex-row gap-5 items-center justify-center'>
+        //                 <li><img src="./assets/instagram_logo.svg" alt="" /></li>
+        //                 <li><img src="./assets/facebook_logo.svg" alt="" /></li>
+        //                 <li><img src="./assets/twitter_logo.svg" alt="" /></li>
+        //             </ul>
+        //         </div>
+        //     </div>
+        //     <section className='flex flex-col justify-center items-center mb-5 '>
+        //         <h3>Copyright NoCountry 2023. All rights reserved.</h3>
+        //         <span className='flex'><img src='./assets/location_logo.svg'></img>CABA, Argetina</span>
+        //     </section>
+
+
+
+        // </footer>
+
+        <>
+
+            <div className="fill-gray rectangle">
+                <div className="fill-gray semi-circle semi-circle-1"></div>
+                <div className="fill-gray semi-circle semi-circle-2"></div>
             </div>
-        </footer>
+        </>
     )
 }
 
