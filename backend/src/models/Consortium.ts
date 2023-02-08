b2840843c2bd4364fd7cc1b9d74353ad6b3e484a
Adrian <esteban.adrian.gonz@gmail.com>
2023-02-08 00:33:55 -0300

    Added Consortium model

---
+++ backend/src/models/Consortium.ts
@@ -0,0 +1,39 @@
+import { Schema, model } from 'mongoose'
+import { IConsortium } from '../interfaces/consortium'
+
+const consortiumSchema = new Schema<IConsortium>({
+    name: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    users: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ],
+    admin: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    floor: {
+        type: Number,
+        required: true,
+    },
+    apt: {
+        type: Number,
+        required: true,
+    },
+    amenities: [
+        {
+            tpe: Schema.Types.ObjectId,
+            ref: 'Amenity',
+        },
+    ],
+})
+
+export default model('Consortium', consortiumSchema)
