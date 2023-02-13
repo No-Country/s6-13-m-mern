455ccbde80aefb0476d407bf648aa46eaeb18e04
Adrian <esteban.adrian.gonz@gmail.com>
2023-02-13 08:17:06 -0300

    Consortium routes created

--- backend/src/routes/consortiumRoutes.ts
+++ backend/src/routes/consortiumRoutes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createConsortium } from "../controllers/consortium";
+import { createConsortium, getConsortium } from "../controllers/consortium";
+import { addUserConsortium } from "../controllers/consortium/addUserConsortiumController";
 
 const router = Router();
 
+router.get('/get', getConsortium);
 router.post('/create', createConsortium);
+router.post('/add', addUserConsortium);
 router.put('/', );
 router.delete('/', );
 
