8843ff1b97297bbd44c14a80401254a8068a96b4
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-02-16 00:52:42 -0300

    register flow

--- frontend/src/pages/private/Validate.tsx
+++ frontend/src/pages/private/Validate.tsx
@@ -4,9 +4,12 @@ import GridLoader from 'react-spinners/GridLoader'
 import { useParams } from 'react-router'
 import { validateUserAccount } from '../../services/validateUserAccount'
 import { renewValidateTokenAccount } from '../../services/renewValidateTokenAccount'
+import BlueModal from '../../components/modal/BlueModal'
+import { Link } from 'react-router-dom'
 
 const Validate = () => {
   const [response, setResponse] = useState('loading')
+  const [modalOpen, setModalOpen] = useState(false)
   const { token, id } = useParams()
 
   const validate = async (id: string, token: string) => {
@@ -26,7 +29,7 @@ const Validate = () => {
   const handleOnClick = async () => {
     if (id) {
       const resp = await renewValidateTokenAccount(id)
-      if (resp.ok === true) console.log('Se te envio nuevo token por correo') //! Aca deberia mandar pop up que se mando nuevo token y navegar al home para sacarlo de esta ruta
+      if (resp.ok === true) setModalOpen(true)
     }
   }
 
@@ -41,6 +44,12 @@ const Validate = () => {
 
   return (
     <Container>
+      <BlueModal isOpen={modalOpen}>
+        <p>A new token has been sended, check your email to validate your account.</p>
+        <button className="bg-blue text-white text-lg w-14 h-10 rounded-2xl mt-6">
+          <Link to="/">OK</Link>
+        </button>
+      </BlueModal>
       <div className=" h-screen flex items-center justify-center text-center">
         <div className="">
           {response === 'loading' && (
@@ -55,9 +64,7 @@ const Validate = () => {
               <h2 className="text-blueDark text-2xl font-bold">Token Has Expired</h2>
               <button
                 className="bg-blueDark text-white text-xl w-60 h-12 rounded-2xl mb-5"
-                onClick={async () => {
-                  await handleOnClick()
-                }}
+                onClick={handleOnClick}
               >
                 RETRY
               </button>
