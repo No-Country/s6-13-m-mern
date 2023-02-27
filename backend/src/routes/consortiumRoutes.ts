8c33075c7af5df8d1c678f81d864efe9a940d740
pablo-zalazar <pablo_zalazar@outlook.com>
2023-02-27 18:13:25 -0300

    edit consortium endpoint

--- backend/src/routes/consortiumRoutes.ts
+++ backend/src/routes/consortiumRoutes.ts
@@ -6,13 +6,26 @@ import {
     addUserConsortium,
     addAmenityConsortium,
     deleteConsortiumUser,
+    editConsortiumController,
 } from '../controllers/consortium'
+import {
+    compareIds,
+    updateConsortiumValidate,
+    validateAdmin,
+} from '../middlewares'
 
 const router = Router()
 
 router.get('/get/:id', getConsortium)
 router.post('/create', createConsortium)
 router.put('/addUser/:consortiumId/:userId', addUserConsortium)
+router.put(
+    '/editConsortium/:consortiumId/:id',
+    validateAdmin,
+    compareIds,
+    updateConsortiumValidate,
+    editConsortiumController
+)
 router.put('/add/:consortiumId/:amenityId', addAmenityConsortium)
 router.put('/removeUser/:consortiumId/:userId', deleteConsortiumUser)
 router.delete('/delete/:consortiumId', deleteConsortium)
