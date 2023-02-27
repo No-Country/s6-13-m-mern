194a565327117828dc1321e84e5add503cad9465
pablo-zalazar <pablo_zalazar@outlook.com>
2023-02-27 13:42:43 -0300

    routes fixes

--- backend/src/controllers/consortium/createConsortiumController.ts
+++ backend/src/controllers/consortium/createConsortiumController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express'
+import { IResponse } from '../../interfaces'
 import { createConsortiumService } from '../../services/consortium'
 
 export const createConsortium = async (req: Request, res: Response) => {
