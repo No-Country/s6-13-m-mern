34079a2065f163d625152e9b84837917d5e0c266
pablo-zalazar <pablo_zalazar@outlook.com>
2023-02-27 13:39:41 -0300

    routes fixes

--- backend/src/controllers/consortium/createConsortiumController.ts
+++ backend/src/controllers/consortium/createConsortiumController.ts
@@ -1,14 +1,11 @@
 import { Request, Response } from 'express'
 import { createConsortiumService } from '../../services/consortium'
-import { IResponse } from '../../interfaces/response'
 
 export const createConsortium = async (req: Request, res: Response) => {
-    console.log(req.body)
     try {
-        const { ok, status, consortium, error } =
-            (await createConsortiumService(req.body)) as IResponse
-
-        console.log(ok, error)
+        const { ok, status, consortium, error } = await createConsortiumService(
+            req.body
+        )
 
         if (!ok) return res.status(400).json({ error })
         return res.status(status).json(consortium)
