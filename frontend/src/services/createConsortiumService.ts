239461408be6cc6b4af417cc100b03d510e4f7a2
Javier Morón <requin883@gmail.com>
2023-02-26 17:26:53 -0800

    Update createConsortiumService

--- frontend/src/services/createConsortiumService.ts
+++ frontend/src/services/createConsortiumService.ts
@@ -1,14 +1,16 @@
 import instance from '../axios/axiosInstance'
+import { ConsortiumCreationValues } from '../interfaces/amenitiesInterfaces'
 
 export const getAllAmenitiesService = async () => {
   try {
     const allAmenities = await instance.get('api/amenity/id/all')
-    return allAmenities
+    return allAmenities.data.amenityRetrieved
   } catch (err) {
     console.log(err)
   }
 }
 
-export const createConsortiumService(data){
-  const response = await instance.post('api/amenity')
+export const createConsortiumService = async (data: ConsortiumCreationValues) => {
+  const response = await instance.post('api/consortium/create', { data })
+  return response
 }
