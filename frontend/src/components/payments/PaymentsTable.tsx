81d4faffb03552938c7e2e07efc901cab8152eb2
MartinKun <martinkunbrc@hotmail.com>
2023-02-27 05:03:02 -0300

    connected ConsortiumPayments to API

--- frontend/src/components/payments/PaymentsTable.tsx
+++ frontend/src/components/payments/PaymentsTable.tsx
@@ -1,30 +1,51 @@
+import { useEffect } from 'react'
 import { PaymentsValues } from '../../interfaces/paymentsInterfaces'
 
 interface Props {
   data: PaymentsValues[]
   setData: React.Dispatch<React.SetStateAction<PaymentsValues[]>>
-  role: string | undefined
+  dataToCompare: PaymentsValues[]
   filterValue: string
+  setIsDataModified: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const PaymentsTable = ({ data, setData, role, filterValue }: Props) => {
+const PaymentsTable = ({ data, setData, dataToCompare, filterValue, setIsDataModified }: Props) => {
   const headers = ['Name', 'Vouchers', 'Amount', 'Method', 'Status']
 
   const selectOption = (e: React.ChangeEvent<HTMLSelectElement>, index: number) => {
     setData((prevData) =>
       prevData.map((item, i) => {
         if (i === index) {
-          return { ...item, status: e.target.value }
+          return { ...item, pStatus: e.target.value }
         }
         return item
       }),
     )
   }
 
+  useEffect(() => {
+    if (isDataModified()) {
+      setIsDataModified(true)
+    } else {
+      setIsDataModified(false)
+    }
+  }, [data])
+
+  const isDataModified = () => {
+    let isModified = false
+    data.forEach((item) => {
+      const sameIdItem = dataToCompare.find((itemToCompare) => item._id === itemToCompare._id)
+      if (sameIdItem?.pStatus !== item.pStatus) {
+        isModified = true
+      }
+    })
+    return isModified
+  }
+
   const sortedData = (data: PaymentsValues[]) => {
     return data.sort((a, b) => {
-      const statusOrder = ['Pending', 'Denied', 'Validated']
-      return statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status)
+      const statusOrder = ['pending', 'denied', 'validated']
+      return statusOrder.indexOf(a.pStatus) - statusOrder.indexOf(b.pStatus)
     })
   }
 
@@ -44,50 +65,41 @@ const PaymentsTable = ({ data, setData, role, filterValue }: Props) => {
       </thead>
       <tbody>
         {sortedData(data)
-          .filter((row) => row.name.includes(filterValue))
+          .filter((row) => row.user.name.toLocaleLowerCase().includes(filterValue.toLowerCase()))
           .map((row: PaymentsValues, index: number) => (
             <tr
-              key={row.id_}
+              key={row._id}
               className={`${index !== data.length - 1 ? 'border-b-[1px] border-greyLight' : ''}`}
             >
-              <td className="pt-5 py-2">{row.name}</td>
+              <td className="pt-5 py-2">{row.user.name}</td>
               <td className="pt-5 py-2 text-blue">
                 <a
                   target="_blank"
-                  href={row.vouchers}
+                  href={row.image}
                   className="underline decoration-1 hover:text-violet ease-out duration-300"
                   rel="noreferrer"
                 >
-                  {row.vouchers.split('/')[row.vouchers.split('/').length - 1]}
+                  {row.image.split('/')[row.image.split('/').length - 1]}
                 </a>
               </td>
-              <td className="pt-5 py-2">{row.amount}</td>
-              <td className="pt-5 py-2">{row.method}</td>
-              {role === 'user' && (
-                <td
-                  className="pt-5 py-2"
-                  key={row.id_}
+              <td className="pt-5 py-2">{row.ammount}</td>
+              <td className="pt-5 py-2">{row.paymentMethod}</td>
+
+              <td
+                className="pt-5 py-2"
+                key={row._id}
+              >
+                <select
+                  onChange={(e) => {
+                    selectOption(e, index)
+                  }}
+                  value={row.pStatus}
                 >
-                  {row.status}
-                </td>
-              )}
-              {role === 'admin' && (
-                <td
-                  className="pt-5 py-2"
-                  key={row.id_}
-                >
-                  <select
-                    onChange={(e) => {
-                      selectOption(e, index)
-                    }}
-                    value={row.status}
-                  >
-                    <option value="Pending">Pending</option>
-                    <option value="Denied">Denied</option>
-                    <option value="Validated">Validated</option>
-                  </select>
-                </td>
-              )}
+                  <option value="pending">Pending</option>
+                  <option value="denied">Denied</option>
+                  <option value="validated">Validated</option>
+                </select>
+              </td>
             </tr>
           ))}
       </tbody>
