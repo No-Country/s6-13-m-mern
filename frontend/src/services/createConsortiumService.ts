98f56a9ec32e60ea8bc21790fc26891402c86fef
MartinKun <martinkunbrc@hotmail.com>
2023-02-28 16:32:28 -0300

    add header token to services

--- frontend/src/services/createConsortiumService.ts
+++ frontend/src/services/createConsortiumService.ts
@@ -1,9 +1,15 @@
 import instance from '../axios/axiosInstance'
 import { ConsortiumCreationValues } from '../interfaces/amenitiesInterfaces'
+import { useAuthStore } from '../store/auth'
 
+const token = useAuthStore.getState().token
 export const getAllAmenitiesService = async () => {
   try {
-    const allAmenities = await instance.get('api/amenity/id/all')
+    const allAmenities = await instance.get('api/amenity/id/all', {
+      headers: {
+        token: `${token}`,
+      },
+    })
     return allAmenities.data.amenityRetrieved
   } catch (err) {
     console.log(err)
@@ -12,7 +18,11 @@ export const getAllAmenitiesService = async () => {
 
 export const createConsortiumService = async (data: ConsortiumCreationValues) => {
   try {
-    const response = await instance.post('api/consortium/create', data)
+    const response = await instance.post('api/consortium/create', data, {
+      headers: {
+        token: `${token}`,
+      },
+    })
     return response
   } catch (err) {
     console.log(err)
@@ -20,7 +30,11 @@ export const createConsortiumService = async (data: ConsortiumCreationValues) =>
 }
 
 export const getConsortiumAmenities = async (consortiumId: string) => {
-  const consortiumAmenities = await instance.get(`api/amenity/${consortiumId}`)
+  const consortiumAmenities = await instance.get(`api/amenity/${consortiumId}`, {
+    headers: {
+      token: `${token}`,
+    },
+  })
   console.log(consortiumAmenities)
 }
 // export editConsortiumService = async (data: any){
