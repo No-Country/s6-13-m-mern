d05ee06e1ff840744f8502819e15bddab887661e
pablo-zalazar <pablo_zalazar@outlook.com>
2023-02-28 14:16:16 -0300

    edit user fix

--- backend/src/controllers/consortium/getConsortiumController.ts
+++ backend/src/controllers/consortium/getConsortiumController.ts
@@ -1,16 +1,17 @@
-import { Request, Response } from 'express';
-import { getConsortiumService } from '../../services/consortium';
-import { IResponse } from '../../interfaces';
-
+import { Request, Response } from 'express'
+import { getConsortiumService } from '../../services/consortium'
+import { IResponse } from '../../interfaces'
 
 export const getConsortium = async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const consortiumRetrieved = (await getConsortiumService(id)) as IResponse;
+    const { id } = req.params
+    try {
+        const consortiumRetrieved = (await getConsortiumService(
+            id
+        )) as IResponse
 
-    const {status} = consortiumRetrieved;
-      return res.status(status).json(consortiumRetrieved);
-  } catch (error) {
-    return res.status(500).json({ ok: false, error});
-  }
-};
+        const { status } = consortiumRetrieved
+        return res.status(status).json(consortiumRetrieved)
+    } catch (error) {
+        return res.status(500).json({ ok: false, error })
+    }
+}
