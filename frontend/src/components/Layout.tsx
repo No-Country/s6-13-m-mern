77f5746c1b16b605a142269329f4691aee1dc55c
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-03-01 02:21:43 -0300

    cambios varios flow2

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -7,25 +7,18 @@ import useScreenSize from '../hooks/useScreenSize'
 const Layout = () => {
   const { width } = useScreenSize()
   return (
-      <div className="min-h-screen flex flex-col ">
-        {/* <div className="min-h-fit sm:h-20"> */}
-          <Header />
-        {/* </div> */}
-          <Outlet />
-        { width > 640 ? (
-          <>
-            <div className=" sm:block ">
-              <Footer />
-            </div>
-          </>
-        ) : (
-          <>
-            <div className=' fixed bottom-0 right-1/2 translate-x-1/2 z-50'>
-              <MobileBar />
-            </div>
-          </>
-        )}
+    <div className="min-h-screen flex flex-col ">
+      <div className="min-h-fit sm:h-20">
+        <Header />
       </div>
+      <Outlet />
+      <div className=" sm:block ">
+        <Footer />
+      </div>
+      <div className=" fixed bottom-0 right-1/2 translate-x-1/2 z-50">
+        <MobileBar />
+      </div>
+    </div>
   )
 }
 
