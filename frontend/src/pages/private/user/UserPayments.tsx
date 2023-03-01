1ad3eac2a2c91162de21cf1a4b748d00cc541147
MartinKun <martinkunbrc@hotmail.com>
2023-03-01 02:26:11 -0300

    change ok value in createPayment service (backend) and userPayments refactor

--- frontend/src/pages/private/user/UserPayments.tsx
+++ frontend/src/pages/private/user/UserPayments.tsx
@@ -33,6 +33,8 @@ const UserDocuments = () => {
   const [loadingPayments, setLoadingPayments] = useState(false)
   const [errorGetPayments, setErrorGetPayments] = useState(false)
   const userId = useAuthStore((state) => state.id)
+  const [modal, setModal] = useState(false)
+  const [paymentDetail, setPaymentDetail] = useState<PaymentsValues>()
 
   useEffect(() => {
     setLoadingPayments(true)
@@ -114,9 +116,15 @@ const UserDocuments = () => {
     )
   }
 
+  const handleClickDetail = (payment: PaymentsValues) => {
+    setPaymentDetail(payment)
+    setModal(true)
+  }
+
   const renderPayments = () => {
     return (
       <div>
+        {modal && renderModal()}
         <div className="-translate-y-5 w-full flex justify-center">
           <div className="w-full ml-[10%]">
             <SearchBar
@@ -136,26 +144,48 @@ const UserDocuments = () => {
             {!Array.isArray(data) || data.length === 0 ? (
               <h3>There are no results to show.</h3>
             ) : (
-              data.map((document) => (
-                <a
-                  key={document._id}
-                  className="h-[140px] flex flex-col
+              data.map((document) =>
+                document.paymentMethod === 'transfer' ? (
+                  <a
+                    key={document._id}
+                    className="h-[140px] flex flex-col
                           items-center justify-between
                           border-2 border-black
                           rounded-xl bg-white
                           pt-4 pb-2 font-inter"
-                  href={document.image}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <h4 className="font-bold">{`VOUCHER - ${convertDate(document.creationDate)}`}</h4>
-                  <img
-                    src="../assets/Pdf.svg"
-                    className="w-[32px]"
-                  />
-                  <p>{document.user.name}</p>
-                </a>
-              ))
+                    href={document.image}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <h4 className="font-bold">{`VOUCHER - ${convertDate(document.creationDate)}`}</h4>
+                    <img
+                      src="../assets/Pdf.svg"
+                      className="w-[32px]"
+                    />
+                    <p>{document.user.name}</p>
+                  </a>
+                ) : (
+                  <div
+                    key={document._id}
+                    className="h-[140px] flex flex-col
+                items-center justify-between
+                border-2 border-black
+                rounded-xl bg-white
+                pt-4 pb-2 font-inter"
+                  >
+                    <h4 className="font-bold">{convertDate(document.creationDate)}</h4>
+                    <h4 className="italic">Cash - ${document.ammount}</h4>
+                    <button
+                      onClick={() => {
+                        handleClickDetail(document)
+                      }}
+                      className="text-[#007bff] underline font-bold"
+                    >
+                      Detail
+                    </button>
+                  </div>
+                ),
+              )
             )}
           </div>
         </div>
@@ -173,10 +203,43 @@ const UserDocuments = () => {
     )
   }
 
+  const renderModal = () => {
+    return (
+      <div className="fixed inset-0 flex justify-center items-center bg-blue bg-opacity-50 z-50">
+        <div className="animate-slideInTop relative w-[560px] h-[347px] rounded-xl border-2 border-blueDark flex flex-col justify-center items-center bg-white">
+          <div
+            onClick={() => {
+              setModal(false)
+            }}
+            className="absolute right-5 top-5 cursor-pointer"
+          >
+            <img src="https://res.cloudinary.com/dozwd1ssj/image/upload/v1677639798/close-outline_b6olmb.png" />
+          </div>
+          <div className="w-full h-full flex flex-col justify-between items-center px-10 py-16">
+            <h4 className="font-bold text-blueDark">
+              {paymentDetail?.user?.name && paymentDetail?.user?.lastname && paymentDetail?.creationDate
+                ? `by ${paymentDetail.user.name} ${paymentDetail.user.lastname} (${convertDate(
+                    paymentDetail.creationDate,
+                  )})`
+                : ''}
+            </h4>
+            <h4 className="italic font-bold">Cash - ${paymentDetail?.ammount}</h4>
+            <p className="w-full">
+              <span className="font-bold pb-2 w-full">
+                Note: <br />
+              </span>
+              {paymentDetail?.note}
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       {!create ? (
-        <div>
+        <div className="relative">
           <TitleComponents title="Payments" />
           {loadingPayments && renderLoading()}
           {!loadingPayments && errorGetPayments && renderError()}
