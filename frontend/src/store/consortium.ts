c0cb689997bd3f2d070802226818cebb1faf7f03
Bellantra <yamilapaez2@hotmail.com>
2023-03-04 21:38:41 +0100

    zustand options updated

--- frontend/src/store/consortium.ts
+++ frontend/src/store/consortium.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 import { ConsortiaData } from '../interfaces/consortiaInterfaces'
 
 interface State {
@@ -9,11 +10,23 @@ interface Actions {
   setData: (consortiumData: ConsortiaData) => void
 }
 
-export const useConsortiumStore = create<State & Actions>((set) => ({
-  consortiumData: null,
-  setData: (consortiumData: ConsortiaData) => {
-    set((state) => ({
-      consortiumData
-    }))
-  },
-}))
+export const useConsortiumStore = create<State & Actions>()(
+  devtools(
+    (set) => ({
+      consortiumData: null,
+      setData: (consortiumData: ConsortiaData) => {
+        set((state) => ({
+          consortiumData,
+        }))
+      },
+    }),
+    {
+      name: 'consortium',
+      enabled: true,
+      anonymousActionType: 'ConsortiumAction',
+      serialize: {
+        options: true,
+      },
+    },
+  ),
+)
