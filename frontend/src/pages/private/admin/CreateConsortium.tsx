850ddfc4f11f5f95e767adbd974a5da06d8f93e4
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-03-06 14:04:27 -0300

    consortium create1

--- frontend/src/pages/private/admin/CreateConsortium.tsx
+++ frontend/src/pages/private/admin/CreateConsortium.tsx
@@ -13,6 +13,10 @@ import { PulseLoader } from 'react-spinners'
 import BlueModal from '../../../components/modal/BlueModal'
 import { useTitle } from '../../../store/title'
 import ImageUploader from '../../../components/ImageUploader'
+import { useNavigate } from 'react-router-dom'
+import getUserByIdService from '../../../services/getUserByIdService'
+import { IResponseUser } from '../../../interfaces/userInterfaces'
+import { userStore } from '../../../store/user'
 
 interface Props {
   setMenu: React.Dispatch<React.SetStateAction<string>>
@@ -23,6 +27,9 @@ const defaultImage = 'https://res.cloudinary.com/dozwd1ssj/image/upload/v1677157
 const CreateConsortium = ({ setMenu }: Props) => {
   const userId = useAuthStore((state) => state.id)
   const [image, setImage] = useState<string | undefined>(defaultImage)
+
+  const navigate = useNavigate()
+  const setUser = userStore((state) => state.setData)
 
   const setTitle = useTitle((state) => state.setTitle)
   setTitle('Create consortium')
@@ -65,6 +72,15 @@ const CreateConsortium = ({ setMenu }: Props) => {
     })
   }, [])
 
+  const getUser = async () => {
+    try {
+      const res = (await getUserByIdService(userId)) as IResponseUser
+      setUser(res.user)
+    } catch (error) {
+      console.log('error')
+    }
+  }
+
   const customSubmit = async (data: FormValues) => {
     try {
       setState({ ...state, load: true })
@@ -83,7 +99,8 @@ const CreateConsortium = ({ setMenu }: Props) => {
       // setConsortium(userConsortiumValues)
       console.log(response)
       setState({ ...state, openModal: true, load: false })
-      location.reload()
+      // eslint-disable-next-line @typescript-eslint/no-floating-promises
+      getUser()
     } catch (error) {
       setState({
         ...state,
@@ -104,6 +121,7 @@ const CreateConsortium = ({ setMenu }: Props) => {
         <button
           onClick={() => {
             setState({ ...state, openModal: false })
+            navigate('/admin', { state: { show: 'My consortiums' } })
           }}
           className="bg-blue text-white text-lg w-14 h-10 rounded-2xl mt-6"
         >
@@ -180,83 +198,82 @@ const CreateConsortium = ({ setMenu }: Props) => {
                   height={160}
                 />
               </div>
-              <div className='sm:w-[45%] mx-auto'>
-
-              <input
-                className={`border-2 ${
-                  !errors.name ? 'border-blueDark' : 'border-red'
-                } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
-                type="text"
-                autoComplete="off"
-                placeholder="Name"
-                {...register('name', {
-                  required: true,
-                })}
-              />
-              <input
-                className={`border-2 ${
-                  !errors.address ? 'border-blueDark' : 'border-red'
-                } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
-                type="text"
-                autoComplete="off"
-                placeholder="Address"
-                {...register('address', {
-                  required: true,
-                })}
-              />
-              <input
-                className={`border-2 ${
-                  !errors.floor ? 'border-blueDark' : 'border-red'
-                } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
-                type="number"
-                autoComplete="off"
-                placeholder="Floors"
-                {...register('floor', {
-                  required: true,
-                  max: 100,
-                  min: 1,
-                })}
-              />
-              <input
-                className={`border-2 ${
-                  !errors.apt ? 'border-blueDark' : 'border-red'
-                } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
-                type="number"
-                autoComplete="off"
-                placeholder="Units"
-                {...register('apt', {
-                  required: true,
-                  max: 1000,
-                  min: 1,
-                })}
-              />
-              <Controller
-                control={control}
-                name="amenities"
-                rules={{ required: true }}
-                render={({ field: { value, name, onChange } }) => (
-                  <Select
-                    styles={{
-                      control: (baseStyles, state) => ({
-                        ...baseStyles,
-                      }),
-                    }}
-                    onChange={onChange}
-                    options={state.amenitiesList}
-                    isMulti={true}
-                    value={value}
-                    name={name}
-                  />
-                )}
-              />
-              <button
-                disabled={!isDirty || !isValid || state.load}
-                className="w-full mt-10 disabled:bg-gray-500 text-center text-white rounded-md px-10 py-3 bg-blueDark"
-                type="submit"
-              >
-                {state.load ? <PulseLoader color="white" /> : 'Continue'}
-              </button>
-                  </div>
+              <div className="sm:w-[45%] mx-auto">
+                <input
+                  className={`border-2 ${
+                    !errors.name ? 'border-blueDark' : 'border-red'
+                  } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
+                  type="text"
+                  autoComplete="off"
+                  placeholder="Name"
+                  {...register('name', {
+                    required: true,
+                  })}
+                />
+                <input
+                  className={`border-2 ${
+                    !errors.address ? 'border-blueDark' : 'border-red'
+                  } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
+                  type="text"
+                  autoComplete="off"
+                  placeholder="Address"
+                  {...register('address', {
+                    required: true,
+                  })}
+                />
+                <input
+                  className={`border-2 ${
+                    !errors.floor ? 'border-blueDark' : 'border-red'
+                  } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
+                  type="number"
+                  autoComplete="off"
+                  placeholder="Floors"
+                  {...register('floor', {
+                    required: true,
+                    max: 100,
+                    min: 1,
+                  })}
+                />
+                <input
+                  className={`border-2 ${
+                    !errors.apt ? 'border-blueDark' : 'border-red'
+                  } rounded-lg h-12 px-4 mb-8 w-full placeholder:italic placeholder:text-grey bg-white focus:outline-none text-lg`}
+                  type="number"
+                  autoComplete="off"
+                  placeholder="Units"
+                  {...register('apt', {
+                    required: true,
+                    max: 1000,
+                    min: 1,
+                  })}
+                />
+                <Controller
+                  control={control}
+                  name="amenities"
+                  rules={{ required: true }}
+                  render={({ field: { value, name, onChange } }) => (
+                    <Select
+                      styles={{
+                        control: (baseStyles, state) => ({
+                          ...baseStyles,
+                        }),
+                      }}
+                      onChange={onChange}
+                      options={state.amenitiesList}
+                      isMulti={true}
+                      value={value}
+                      name={name}
+                    />
+                  )}
+                />
+                <button
+                  disabled={!isDirty || !isValid || state.load}
+                  className="w-full mt-10 disabled:bg-gray-500 text-center text-white rounded-md px-10 py-3 bg-blueDark"
+                  type="submit"
+                >
+                  {state.load ? <PulseLoader color="white" /> : 'Continue'}
+                </button>
+              </div>
             </form>
           </div>
         </div>
